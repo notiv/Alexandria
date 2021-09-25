939bc8339b4e793dfae561bdd7c3b58768b1a719
Salvatore Cosseddu <salvatore.cosseddu@gmail.com>
2021-09-25 23:26:02 +0200

    .

--- alexandria/ocr.py
+++ alexandria/ocr.py
@@ -57,10 +57,22 @@ def match_template(image, template):
     return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
 # ------------------------------------------------------------------
 
-def preprocess4ocr(image):
-    img = get_grayscale(image)
+def preprocess4ocr(img):
+    img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    img = get_grayscale(img)
     img = thresholding(img)
     return opening(img)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # kernel = np.ones((1, 1), np.uint8)
+    # img = cv2.dilate(img, kernel, iterations=1)
+    # img = cv2.erode(img, kernel, iterations=1)
+    #img = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+#    cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+#    cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+#    cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+#    cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    #img = cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    return img
 
 def image_to_text(img):
     return pytesseract.image_to_data(img, output_type=Output.DICT).get('text')
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from alexandria import detection, ocr, post_processing
 from tqdm.notebook import tqdm
 import warnings
 
-warnings.filterwarnings("default")
+warnings.filterwarnings("ignore")
 # user input
 confidence = 0.5
 api_key = "../api_key.txt"
@@ -15,6 +15,7 @@ api_key = post_processing.get_api_key(api_key)
 classes, colors = detection.load_classes("coco.names")
 model = detection.load_model(cfg='yolov3.cfg', model='yolov3.weights')
 images_paths, images_list = detection.load_images()
+# images_paths, images_list = images_paths[:1], images_list[:1]
 outputs_list = [detection.detect(i, model) for i in images_list]
 boxes_positions = [detection.get_boxes(i, o,
                     confidence, classes, colors)
@@ -28,25 +29,62 @@ def get_titles(id, img):
     out["id"] = id
     out["book_image"] = img
     out["title_from_ocr"] = " ".join(ocr.image_to_text(img))
-    out["cleaned_titles"] = post_processing.search_book(out["title_from_ocr"], api_key)
+    out["cleaned_titles"] = post_processing.clean_up_text(out["title_from_ocr"])
+    out["final_titles"] = post_processing.search_book(out["cleaned_titles"], api_key)
     return out
 
 books_text = {}
 for path, image, boxes in tqdm(zip(images_paths, images_list, boxes_positions), total=len(images_paths)):
     books_text[path] = []
-    sub_images = ocr.get_boxes_per_image(image, boxes)
+    sub_images = ocr.get_boxes_per_image(
+        image=ocr.preprocess4ocr(image),
+        boxes=boxes)
 
     for n, i in enumerate(sub_images):
         try:
             books_text[path].append(get_titles(n, i))
-        except ValueError:
-            warnings.warn(f"{path}: {n} box is discarded as on the edge")
+        except Exception:
+            warnings.warn(f"{path}: {n} box is discarded")
 
 
 for k, v in books_text.items():
     print(k)
     for i in v:
-        print(i["title_from_ocr"])
-        print(i["cleaned_titles"])
+        print("orig", i["title_from_ocr"])
+        print("clean", i["cleaned_titles"])
+        print("final", i["final_titles"])
 
+# # plt.imshow(ocr.preprocess4ocr(image))
+# # image.shape
+# # ocr.preprocess4ocr(image).shape
+
+
+# # from autocorrect import Speller
+
+# # spell = Speller(only_replacements=True)
+# # w = spell("fournier tpeilly deep learning coders with fastaij")
+# # post_processing.search_book(w, api_key)
+
+# # [spell(i) for i in ["fournier", "tpeilly", "deep", "learning", "coders", "with", "fastaij"]]
+
+# # print("orig", i["title_from_ocr"])
+# # print("clean", i["cleaned_titles"])
+# # print("final", i["final_titles"])
+
+# # from textblob import TextBlob
+
+# # ww = TextBlob("inas datubase internals petrov oreilly")
+
+# # from alexandria import detection, ocr, post_processing
+# # api_key = "../api_key.txt"
+# # api_key = post_processing.get_api_key(api_key)
+# # post_processing.search_book("inns database internal petrov oreille", api_key)
+
+
+# # inns database internal petrov oreille
+
+
+# # import textcleaner as tc
+
+# # help(tc.document)
 
