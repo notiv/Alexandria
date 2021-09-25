a0a83a70de454c989a0f71a5c9f12a0145b50e15
Viton Vitanis <960755+notiv@users.noreply.github.com>
2021-09-25 16:32:52 +0200

    Small pytesseract function added

--- alexandria/ocr.py
+++ alexandria/ocr.py
@@ -1,1 +1,11 @@
 import pytesseract
+
+from pytesseract import Output
+
+def image_to_text(img):
+    d = pytesseract.image_to_data(img, output_type=Output.DICT)
+    text = d.get('text')
+
+    return text
+
+ 
